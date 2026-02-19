13e233c2220fb1f6a3c9587cc51fdb65e93eebcf
harshal019 <harshalgharat1918@gmail.com>
2026-02-20 01:50:12 +0530

    Updated

--- scripts/install-jenkins.sh
+++ scripts/install-jenkins.sh
@@ -10,6 +10,18 @@ sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update -y #!/bin/bash
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+
+sudo apt update -y
+sudo apt install maven -y
+
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt update -y 
 sudo apt install jenkins -y 
 
