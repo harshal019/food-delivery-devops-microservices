bf92d8bebe7a87f5064d8f113e49dec3f2dc4a29
harshal019 <harshalgharat1918@gmail.com>
2026-09-09 23:35:21 +0530

    terrform module updated

--- terraform/modules/eks/outputs.tf
+++ terraform/modules/eks/outputs.tf
@@ -1,2 +1,14 @@
-output "cluster_endpoint" { value = aws_eks_cluster.main.endpoint }
-output "cluster_certificate_authority" { value = aws_eks_cluster.main.certificate_authority[0].data }
+output "cluster_endpoint" {
+     value = aws_eks_cluster.main.endpoint 
+     }
+output "cluster_certificate_authority" {
+     value = aws_eks_cluster.main.certificate_authority[0].data 
+     }
+
+output "cluster_name" {
+  value = aws_eks_cluster.main.name
+}
+
+output "oidc_provider_arn" {
+  value = aws_iam_openid_connect_provider.eks.arn
+}
