f3c8c52a8abd447f6e9085de948e81a72b24422c
harshal019 <harshalgharat1918@gmail.com>
2026-09-10 14:29:50 +0530

    terraform   updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -51,15 +51,7 @@ module "eks" {
 }
 
 
-module "aws-load-balancer-controller" {
-  source = "./modules/aws-load-balancer-controller"
 
-  cluster_name = module.eks.cluster_name
-  region       = var.aws_region
-  vpc_id       = module.vpc.vpc_id
-
-  oidc_provider_arn = module.eks.oidc_provider_arn
-}
 
 
 module "jenkins" {
